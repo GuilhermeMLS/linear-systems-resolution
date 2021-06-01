1682aa13a89425add3c0482c51debb59bf2c7c56
Guilherme Morais <guitaikion@gmail.com>
2021-06-01 19:02:46 -0300

    add refinement for every L2Norm above 5.0

--- labSisLin.c
+++ labSisLin.c
@@ -5,6 +5,25 @@
 #include "utils.h"
 #include "SistemasLineares.h"
 
+SistLinear_t *copyLinearSystem(SistLinear_t *linearSystem) {
+    SistLinear_t *copy = alocaSistLinear(linearSystem->n);
+    // Copy matrix A
+    for (int i = 0; i < linearSystem->n; i++) {
+        for (int j = 0; j < linearSystem->n; j++) {
+            copy->A[i][j] = linearSystem->A[i][j];
+        }
+    }
+    // Copy b
+    for (int i = 0; i < linearSystem->n; i++) {
+        copy->b[i] = linearSystem->b[i];
+    }
+    // Copy erro and n
+    copy->erro = linearSystem->erro;
+    copy->n = linearSystem->n;
+    return copy;
+}
+
+
 void printSolution(
     char* method,
     double executionTime,
@@ -25,78 +44,86 @@ void printSolution(
     printf("\n");
 }
 
-void gaussianElimination(SistLinear_t* linearSystem) {
+void refineSolution(SistLinear_t* linearSystem, real_t* solutionArray) {
+    SistLinear_t *refinementLinearSystem = copyLinearSystem(linearSystem);
     double_t* executionTime = malloc(sizeof(double_t));
     *executionTime = timestamp();
-    real_t* solutionArray = malloc(linearSystem->n * sizeof(real_t));
-    eliminacaoGauss(linearSystem, solutionArray, executionTime);
-    real_t* residueArray = malloc(linearSystem->n * sizeof(real_t));
-    real_t residueL2Norm = normaL2Residuo(linearSystem, solutionArray, residueArray);
-    printSolution("Eliminação Gauss", *executionTime, solutionArray, linearSystem->n, residueL2Norm, 0);
+    real_t* refinedSolution = malloc(refinementLinearSystem->n * sizeof(real_t));
+    int numberOfIterations = refinamento(refinementLinearSystem, solutionArray, executionTime);
+    real_t* residueArray = malloc(refinementLinearSystem->n * sizeof(real_t));
+    real_t residueL2Norm = normaL2Residuo(refinementLinearSystem, refinedSolution, residueArray);
+    printSolution("Refinamento", *executionTime, refinedSolution, refinementLinearSystem->n, residueL2Norm, numberOfIterations);
+}
+
+void gaussianElimination(SistLinear_t* linearSystem) {
+    SistLinear_t *gaussEliminationLinearSystem = copyLinearSystem(linearSystem);
+    double_t* executionTime = malloc(sizeof(double_t));
+    *executionTime = timestamp();
+    real_t* solutionArray = malloc(gaussEliminationLinearSystem->n * sizeof(real_t));
+    eliminacaoGauss(gaussEliminationLinearSystem, solutionArray, executionTime);
+    real_t* residueArray = malloc(gaussEliminationLinearSystem->n * sizeof(real_t));
+    real_t residueL2Norm = normaL2Residuo(gaussEliminationLinearSystem, solutionArray, residueArray);
+    printSolution("Eliminação Gauss", *executionTime, solutionArray, gaussEliminationLinearSystem->n, residueL2Norm, 0);
+    if (residueL2Norm > 5.0) {
+        refineSolution(gaussEliminationLinearSystem, solutionArray);
+    }
 }
 
 void jacobiMethod(SistLinear_t* linearSystem) {
+    SistLinear_t *jacobiMethodLinearSystem = copyLinearSystem(linearSystem);
     double_t* executionTime = malloc(sizeof(double_t));
     *executionTime = timestamp();
-    real_t* solutionArray = malloc(linearSystem->n * sizeof(real_t));
-    int numberOfIterations = gaussJacobi(linearSystem, solutionArray, executionTime);
-    real_t* residueArray = malloc(linearSystem->n * sizeof(real_t));
-    real_t residueL2Norm = normaL2Residuo(linearSystem, solutionArray, residueArray);
-    printSolution("Jacobi", *executionTime, solutionArray, linearSystem->n, residueL2Norm, numberOfIterations);
+    real_t* solutionArray = malloc(jacobiMethodLinearSystem->n * sizeof(real_t));
+    int numberOfIterations = gaussJacobi(jacobiMethodLinearSystem, solutionArray, executionTime);
+    real_t* residueArray = malloc(jacobiMethodLinearSystem->n * sizeof(real_t));
+    real_t residueL2Norm = normaL2Residuo(jacobiMethodLinearSystem, solutionArray, residueArray);
+    printSolution("Jacobi", *executionTime, solutionArray, jacobiMethodLinearSystem->n, residueL2Norm, numberOfIterations);
+    if (residueL2Norm > 5.0) {
+        refineSolution(jacobiMethodLinearSystem, solutionArray);
+    }
 }
 
 void gaussSeidelMethod(SistLinear_t* linearSystem) {
+    SistLinear_t *gaussSeidelMethodLinearSystem = copyLinearSystem(linearSystem);
     double_t* executionTime = malloc(sizeof(double_t));
     *executionTime = timestamp();
-    real_t* solutionArray = malloc(linearSystem->n * sizeof(real_t));
-    int numberOfIterations = gaussSeidel(linearSystem, solutionArray, executionTime);
-    real_t* residueArray = malloc(linearSystem->n * sizeof(real_t));
-    real_t residueL2Norm = normaL2Residuo(linearSystem, solutionArray, residueArray);
-    printSolution("Gauss-Seidel", *executionTime, solutionArray, linearSystem->n, residueL2Norm, numberOfIterations);
+    real_t* solutionArray = malloc(gaussSeidelMethodLinearSystem->n * sizeof(real_t));
+    int numberOfIterations = gaussSeidel(gaussSeidelMethodLinearSystem, solutionArray, executionTime);
+    real_t* residueArray = malloc(gaussSeidelMethodLinearSystem->n * sizeof(real_t));
+    real_t residueL2Norm = normaL2Residuo(gaussSeidelMethodLinearSystem, solutionArray, residueArray);
+    printSolution("Gauss-Seidel", *executionTime, solutionArray, gaussSeidelMethodLinearSystem->n, residueL2Norm, numberOfIterations);
+    if (residueL2Norm > 5.0) {
+        refineSolution(gaussSeidelMethodLinearSystem, solutionArray);
+    }
 }
 
 void gaussianEliminationWithRefinementMethod(SistLinear_t* linearSystem) {
+    SistLinear_t *gaussianEliminationWithRefinementLinearSystem = copyLinearSystem(linearSystem);
     double_t* executionTime = malloc(sizeof(double_t));
     *executionTime = timestamp();
-    real_t* solutionArray = malloc(linearSystem->n * sizeof(real_t));
-    int numberOfIterations = refinamento(linearSystem, solutionArray, executionTime);
-    real_t* residueArray = malloc(linearSystem->n * sizeof(real_t));
-    real_t residueL2Norm = normaL2Residuo(linearSystem, solutionArray, residueArray);
-    printSolution("Refinamento", *executionTime, solutionArray, linearSystem->n, residueL2Norm, numberOfIterations);
+    real_t* solutionArray = malloc(gaussianEliminationWithRefinementLinearSystem->n * sizeof(real_t));
+    int numberOfIterations = refinamento(gaussianEliminationWithRefinementLinearSystem, solutionArray, executionTime);
+    real_t* residueArray = malloc(gaussianEliminationWithRefinementLinearSystem->n * sizeof(real_t));
+    real_t residueL2Norm = normaL2Residuo(gaussianEliminationWithRefinementLinearSystem, solutionArray, residueArray);
+    printSolution("Refinamento", *executionTime, solutionArray, gaussianEliminationWithRefinementLinearSystem->n, residueL2Norm, numberOfIterations);
+    if (residueL2Norm > 5.0) {
+        refineSolution(gaussianEliminationWithRefinementLinearSystem, solutionArray);
+    }
 }
 
 int hasLinearSystemToRead(int *numberOfElements) {
     return scanf("%d", numberOfElements) != EOF;
 }
 
-SistLinear_t *copyLinearSystem(SistLinear_t *linearSystem) {
-    SistLinear_t *copy = alocaSistLinear(linearSystem->n);
-    // Copy matrix A
-    for (int i = 0; i < linearSystem->n; i++) {
-        for (int j = 0; j < linearSystem->n; j++) {
-            copy->A[i][j] = linearSystem->A[i][j];
-        }
-    }
-    // Copy b
-    for (int i = 0; i < linearSystem->n; i++) {
-        copy->b[i] = linearSystem->b[i];
-    }
-    // Copy erro and n
-    copy->erro = linearSystem->erro;
-    copy->n = linearSystem->n;
-    return copy;
-}
-
 int main() {
     int numberOfElements;
     int linearSystemsCounter = 1;
-    SistLinear_t* linearSystem;
+    SistLinear_t *linearSystem;
     while (hasLinearSystemToRead(&numberOfElements)) {
         scanf("%*c"); // ignores \n
         printf("***** Sistema %d ", linearSystemsCounter);
         linearSystem = lerSistLinear(numberOfElements);
-        SistLinear_t *gaussEliminationLinearSystem = copyLinearSystem(linearSystem);
-        gaussianElimination(gaussEliminationLinearSystem);
+        gaussianElimination(linearSystem);
         jacobiMethod(linearSystem);
         gaussSeidelMethod(linearSystem);
         gaussianEliminationWithRefinementMethod(linearSystem);
